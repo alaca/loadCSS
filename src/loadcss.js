da6c12b9c359bea0fdad826f06ea79a91f735542
alaca <ante.laca@gmail.com>
2017-11-15 18:22:04 +0100

    typo

--- src/loadcss.js
+++ src/loadcss.js
@@ -4,7 +4,6 @@
     'use strict';
 
     var d = w.document;      
-
     // load css
     var loadCSS = function(href, media, callback){
 
@@ -20,36 +19,28 @@
                 callback(link);
             };
         }
-
     }
 
     var supportsPreload = function(){
-
         var list = d.createElement('link').relList;
         if (!list || !list.supports) {
             return false;
         }
-
         return list.supports('preload');
-
     };  
 
     // check if browser doesn't supports preload
     if (!supportsPreload()) {
 
         d.addEventListener('DOMContentLoaded', function(){   
-
             // get all link elements and load them with loadCSS 
             var links = d.getElementsByTagName('link');
 
-            [].forEach.call(links, function(link) {
-
-                if (link.rel === 'preload' && link.getAttribute('as') === 'style') { 
-                    loadCSS(link.href, link.getAttribute('media') || 'all');
-                    link.outerHTML = ''; 
+            for (var i in links) {
+                if (links[i].rel === 'preload' && links[i].getAttribute('as') === 'style') { 
+                    loadCSS(links[i].href, links[i].getAttribute('media') || 'all');
                 }
-
-            });
+            }
 
         }, false);
 
